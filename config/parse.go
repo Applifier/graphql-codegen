3538891fd73903b9fc370805d6e798ee0a6ddd6b
Jaakko Lukkari <jaakko@unity3d.com>
2017-03-01 16:29:46 +0200

    Remove debug

--- config/parse.go
+++ config/parse.go
@@ -1,20 +1,9 @@
 package config
 
-import (
-	"fmt"
-
-	"github.com/hashicorp/hcl"
-)
+import "github.com/hashicorp/hcl"
 
 // Parse parses codegen config
 func Parse(config string) (cfg Config, err error) {
 	err = hcl.Decode(&cfg, config)
-
-	broad := map[string]interface{}{}
-
-	hcl.Decode(&broad, config)
-
-	fmt.Printf("%+v\n", broad)
-
 	return
 }
